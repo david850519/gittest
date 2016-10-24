666623a005b00d5ce0c263c3b7077af498fa0bef
Tim <s911297@gmail.com>
2016-10-24 19:49:51 +0800

    js

---
+++ 123.js
@@ -0,0 +1,9 @@
+function plus(){
+	var str1 = parseInt(document.getElementById("int1").value);
+	var str2 = parseInt(document.getElementById("int2").value);
+		alert(str1+str2);
+}function Multiply(){
+	var str1 = parseInt(document.getElementById("int1").value);
+	var str2 = parseInt(document.getElementById("int2").value);
+		alert(str1*str2);
+}
